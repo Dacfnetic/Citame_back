1a6211f3bb9fb09c43a8f0dace5aedc9945def0b
Dacfnetic <dacf9x@gmail.com>
2024-06-26 10:56:33 -0600

    Cambio al puerto 80

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const { setTimeout } = require('timers')
 
 const server = http.createServer(app)
 const io = new Server(server)
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 80
 const usuariosConectados = new Set()
 const listaDeSocketConCorreo = {}
 const arrayNegocios = []
