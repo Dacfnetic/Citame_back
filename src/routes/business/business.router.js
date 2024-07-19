b6ab60c8251e4f6371d17d5543517384bfae0912
Dacfnetic <dacf9x@gmail.com>
2024-07-19 10:57:02 -0600

    Puntos y comas

--- src/routes/business/business.router.js
+++ src/routes/business/business.router.js
@@ -1,16 +1,16 @@
-const express = require('express')
+const express = require('express');
 const {authMiddleWare} = require('../../middlewares/auth.js');
-const {uploadMiddleware} = require('../../utils/handleStorage.js')
+const {uploadMiddleware} = require('../../utils/handleStorage.js');
 const {resizeImage} = require('../../middlewares/compactImages.js');
-const { getAllBusiness, postBusiness, deleteBusiness, saveChangesFromBusiness } = require('./business.controller.js')
+const { getAllBusiness, postBusiness, deleteBusiness, saveChangesFromBusiness } = require('./business.controller.js');
 
-const businessRouter = express.Router()
+const businessRouter = express.Router();
 
 //Asignación de direcciones
-businessRouter.get('/api/business/get/all', authMiddleWare, getAllBusiness)
-businessRouter.post('/api/business/create', authMiddleWare, uploadMiddleware.single('myfile'), resizeImage, postBusiness)
-businessRouter.delete('/api/business/delete', authMiddleWare, deleteBusiness)
-businessRouter.post('/api/business/saveChangesFromBusiness', uploadMiddleware.fields([{ name: 'imagen', maxCount: 5}]), saveChangesFromBusiness)
+businessRouter.get('/api/business/get/all', authMiddleWare, getAllBusiness);
+businessRouter.post('/api/business/create', authMiddleWare, uploadMiddleware.single('myfile'), resizeImage, postBusiness);
+businessRouter.delete('/api/business/delete', authMiddleWare, deleteBusiness);
+businessRouter.post('/api/business/saveChangesFromBusiness', authMiddleWare, uploadMiddleware.fields([{ name: 'imagen', maxCount: 5}]), saveChangesFromBusiness);
 
 //Exportar enrutador
-module.exports = businessRouter
+module.exports = businessRouter;
--- src/routes/users/users.router.js
+++ src/routes/users/users.router.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const {validatorEmail} = require('../../validators/userValidator.js')
+const {validatorEmail} = require('../../validators/userValidator.js');
 const {authMiddleWare} = require('../../middlewares/auth.js');
-const { getUser, postUser, toggleFavoriteBusiness } = require('./users.controller.js')
+const { getUser, postUser, toggleFavoriteBusiness } = require('./users.controller.js');
 
-const usersRouter = express.Router()
+const usersRouter = express.Router();
 
 //Asignación de direcciones
-usersRouter.get('/api/user/get', authMiddleWare, getUser)
-usersRouter.post('/api/user/create', postUser)
-usersRouter.put('/api/user/favoriteBusiness',authMiddleWare, toggleFavoriteBusiness)
+usersRouter.get('/api/user/get', authMiddleWare, getUser);
+usersRouter.post('/api/user/create', postUser);
+usersRouter.put('/api/user/favoriteBusiness',authMiddleWare, toggleFavoriteBusiness);
 
 //Exportar enrutador
-module.exports = usersRouter
+module.exports = usersRouter;
