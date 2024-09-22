51c025a695bb09363d8b4dbc0b6ea3e9d734a2f9
Dacfnetic <dacf9x@gmail.com>
2024-09-22 17:51:25 -0600

    Comenzando a cambiar hacia bases de datos relacionales

--- src/middlewares/compactImages.js
+++ src/middlewares/compactImages.js
@@ -34,49 +34,26 @@ const resizeImage = async (req,res,next ) =>{
   
   }
 
-  const resizeImages = async (req,res,next ) =>{
+const resizeImages = async (req,res,next ) =>{
    
    try {
-    const{files} = req;
-    
-    let longitud = files.imagen.length;
-  
-     
-    for(let i = 0; i < longitud; i++){
-     
-      const resizedFileName = `resize-${Date.now()}.webp`;
-      const resizedFilePath =`${__dirname}/../storageResized/${resizedFileName}`;
+      const{files} = req;
+      let longitud = files.imagen.length;
+      for(let i = 0; i < longitud; i++){
+        const resizedFileName = `resize-${Date.now()}.webp`;
+        const resizedFilePath =`${__dirname}/../storageResized/${resizedFileName}`;
+        await sharp(files.imagen[i].buffer)
+        .webp()
+        .resize(300) 
+        .toFile(resizedFilePath);
+        req.files.imagen[i].filename = resizedFileName;
+      }
+   }catch{
 
-      await sharp(files.imagen[i].buffer)
-      .webp()
-      .resize(300) 
-      .toFile(resizedFilePath);
-
-      req.files.imagen[i].filename = resizedFileName;
-
-
-
-    }
-
-   
-      
-      
-     
-      
-
-      next();
-
-
-
-   } catch (error) {
-      console.log(error);
-      handleHttpError(res, 'error to resize');
    }
- 
+   next();
+} 
 
-
-}
-  
 module.exports = {resizeImage, resizeImages}
   
 
